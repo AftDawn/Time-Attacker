d5312fbf3456fc43dfb39715f1f82bc171d46b43
AftDawn <42480566+aftdawn@users.noreply.github.com>
2022-03-09 00:35:34 +1100

    First Commit

---
+++ TimeAttackerModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.TimeAttacker {
+
+    [SettingName("Time Attacker - By AftDawn")]
+    public class TimeAttackerSettings : EverestModuleSettings
+    {
+        public bool Enable { get; set; } = false;
+    }
+    
+    public class TimeAttackerModule : EverestModule {
+        
+        public static TimeAttackerModule Instance;
+
+        public TimeAttackerModule()
+        {
+            Instance = this;
+        }
+
+        public static TimeAttackerSettings Settings => (TimeAttackerSettings)Instance._Settings;
+        
+        //public override Type SaveDataType => typeof(TimeAttackerSaveData);
+        //public static TimeAttackerSaveData SaveData => (TimeAttackerSaveData) Instance._SaveData;
+
+        public override void Load()
+        {
+            On.Celeste.SpeedrunTimerDisplay.DrawTime += DrawTime;
+            //On.Celeste.Level.RegisterAreaComplete +=
+        }
+
+        public override void Unload() {
+        }
+
+
+        private void DrawTime(On.Celeste.SpeedrunTimerDisplay.orig_DrawTime orig, Vector2 position, string timeString, float scale, bool valid, bool finished, bool bestTime, float alpha)
+        {
+            if (Settings.Enable && (Engine.Scene as Level) != null)
+            {
+                Level level = Engine.Scene as Level;
+                Session session = level.Session;
+                TimeSpan timeSpan = TimeSpan.FromTicks(session.Time);
+                int time = (int) timeSpan.TotalSeconds;
+            }
+
+        }
+    }
+}
