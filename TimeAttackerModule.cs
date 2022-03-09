66c761fe34805e3c9470c91a4d2053907301c031
AftDawn <DeclanJeffrey6@gmail.com>
2022-03-09 11:40:46 +1100

    just some added stuff, nothing to worry about

--- TimeAttackerModule.cs
+++ TimeAttackerModule.cs
@@ -36,6 +36,7 @@ namespace Celeste.Mod.TimeAttacker {
 
         private void DrawTime(On.Celeste.SpeedrunTimerDisplay.orig_DrawTime orig, Vector2 position, string timeString, float scale, bool valid, bool finished, bool bestTime, float alpha)
         {
+            // if the mod is activated and in a level
             if (Settings.Enable && (Engine.Scene as Level) != null)
             {
                 Level level = Engine.Scene as Level;
@@ -43,7 +44,19 @@ namespace Celeste.Mod.TimeAttacker {
                 TimeSpan timeSpan = TimeSpan.FromTicks(session.Time);
                 int time = (int) timeSpan.TotalSeconds;
             }
+            else
+            {
+                orig(position, timeString, scale, valid, finished, bestTime, alpha);
+            }
 
         }
+        // small helper code and shit idk lol
+        private string formatTime(int seconds)
+        {
+            int remainer = seconds % 60;
+            string time = (seconds / 60) + ":" + (remainer < 10 ? "0" : "") + remainer;
+            return time;
+        }
+        
     }
 }
